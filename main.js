2457b4ba2233e67fc4e1471101526dcc1f298662
unknown <mhughes.ipod@gmail.com>
2020-06-16 19:15:26 -0700

    init

---
+++ main.js
@@ -0,0 +1,121 @@
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
+
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env.NODE_ENV !== 'production'
+const isMac = process.platform === 'darwin'
+
+let mainWindow
+let aboutWindow
+
+function createMainWindow() {
+	mainWindow = new BrowserWindow({
+		//object of options
+		title: 'Image Shrinker',
+		width: 800,
+		height: 600,
+		icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+		resizable: isDev,
+		backgroundColor: 'white',
+	})
+
+	//can load normal URL here too
+	// mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+	mainWindow.loadFile('./app/index.html')
+}
+function createAboutWindow() {
+	aboutWindow = new BrowserWindow({
+		//object of options
+		title: 'Image Shrinker',
+		width: 300,
+		height: 300,
+		icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+		resizable: false,
+		backgroundColor: 'white',
+	})
+
+	//can load normal URL here too
+	// mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+	aboutWindow.loadFile('./app/about.html')
+}
+
+app.on('ready', () => {
+	createMainWindow()
+
+	const mainMenu = Menu.buildFromTemplate(menu)
+	Menu.setApplicationMenu(mainMenu)
+
+	globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
+	globalShortcut.register('CmdOrCtrl+Shift+I', () =>
+		mainWindow.toggleDevTools()
+	)
+
+	mainWindow.on('closed', () => (mainWindow = null))
+})
+
+const menu = [
+	...(isMac
+		? [
+				{
+					label: app.name,
+					submenu: [
+						{
+							label: 'About',
+							click: createAboutWindow,
+						},
+					],
+				},
+		  ]
+		: []),
+	{
+		role: 'fileMenu',
+	},
+	...(!isMac
+		? [
+				{
+					label: 'Help',
+					submenu: [
+						{
+							label: 'About',
+							click: createAboutWindow,
+						},
+					],
+				},
+		  ]
+		: []),
+	...(isDev
+		? [
+				{
+					label: 'Developer',
+					submenu: [
+						{
+							role: 'reload',
+						},
+						{
+							role: 'forcereload',
+						},
+						{
+							type: 'separator',
+						},
+						{
+							role: 'toggledevtools',
+						},
+					],
+				},
+		  ]
+		: []),
+]
+
+app.on('window-all-closed', () => {
+	if (!isMac) {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createMainWindow()
+	}
+})
+
+app.allowRendererProcessReuse = true
